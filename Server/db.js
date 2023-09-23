f77625632d7dafd128795f84680c52a891a8514a
AhmedTLai <139882762+AhmedTLai@users.noreply.github.com>
2023-09-23 04:14:51 +0200

    Update db.js

--- Server/db.js
+++ Server/db.js
@@ -4,7 +4,7 @@ const db = mydb.createConnection({
     host : 'bdxz50worrjjy0e2avmg-mysql.services.clever-cloud.com',
     user : 'uotq5pgtcbyfduqx',
     password : '1uJxwZKMSs26wfD383ZS',
-    database : 'chatapp' 
+    database : 'bdxz50worrjjy0e2avmg' 
 })
  
 
